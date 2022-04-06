9630de77e68eab9137b32726ebbe243d784c4a83
KittenPopo <zlaferte@gmail.com>
2022-04-06 02:29:19 -0700

    Duplicate file removal

--- src/Render/NoteGraphRenderer/NoteGraphRenderer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../Note/NoteGraph/NoteGraph.h"
-#include "../../../Types/Vec.h"
-
-// NoteGraphRender: Responsible for rendering the NoteGraph
-namespace NoteGraphRenderer {
-	void DrawNoteGraph(NoteGraph& graph, Area screenArea);
-}
