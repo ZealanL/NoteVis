b77fc44b577b9fd6068d8010e47f0c8523b059cb
Zealan <zlaferte@gmail.com>
2022-04-13 04:16:42 -0700

    Update (sync) Core.cpp

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -4,6 +4,8 @@
 
 #include "Action/ActionDefines.h"
 #include "../Render/Renderer.h"
+#include "UI/Dialog/Dialog.h"
+#include "UI/UI.h"
 
 auto GetNoteGraphScreenArea() {
 	return Area({ 0,0 }, Renderer::GetWindowSize());
@@ -19,6 +21,8 @@ void Core::OnUserExit() {
 }
 
 void Core::ProcessEvent(SDL_Event& e) {
+	if (UI::PreventInput())
+		return;
 
 	switch (e.type) {
 	case SDL_QUIT:
@@ -39,8 +43,12 @@ void Core::ProcessEvent(SDL_Event& e) {
 		kbFlags |= KBFLAG_ALT * ((keyMods & KMOD_ALT) > 0);
 
 		for (auto& action : g_Actions) {
-			if (action.bind.key == pressedKey && action.bind.flags == kbFlags)
+			if (action.bind.key == pressedKey && action.bind.flags == kbFlags) {
+				if (action.undoable)
+					UpdateHistory();
+
 				action.Execute();
+			}
 		}
 
 		break;
@@ -50,5 +58,58 @@ void Core::ProcessEvent(SDL_Event& e) {
 		break;
 	}
 
+	g_MouseState = SDL_GetMouseState(NULL, NULL);
+
 	g_NoteGraph.UpdateWithInput(GetNoteGraphScreenArea(), e);
+}
+
+deque<ByteDataSteam> noteGraphHistory;
+
+void Core::UpdateHistory() {
+	ByteDataSteam data;
+	g_NoteGraph.Serialize(data);
+
+	if (!noteGraphHistory.empty()) {
+		if (noteGraphHistory.front().DataMatches(data)) {
+			// Prevent storing history when nothing actually changed
+			return;
+		}
+	}
+
+	noteGraphHistory.push_front(data);
+
+	constexpr size_t MAX_HISTORY_SIZE = (1000 * 1000) * 4; // 4 megabytes
+
+	// NOTE: Could store this as a global variable and update it with any changes instead for efficiency, but this is cleaner and simpler
+	size_t totalHistorySize = 0;
+	for (auto history : noteGraphHistory)
+		totalHistorySize += history.size();
+
+	if (totalHistorySize > MAX_HISTORY_SIZE) {
+		DLOG("Undo history size of {}mb exceeds limit of {}mb, removing old history",
+			totalHistorySize / (1000.f * 1000.f),
+			MAX_HISTORY_SIZE / (1000.f * 1000.f)
+		);
+
+		auto& back = noteGraphHistory.back();
+		noteGraphHistory.pop_back();
+	}
+}
+
+bool Core::UndoRestoreHistory() {
+	if (!noteGraphHistory.empty()) {
+		auto& data = noteGraphHistory.front();
+		auto deserializeItr = data.GetIterator();
+
+		g_NoteGraph.ClearEverything();
+		g_NoteGraph.Deserialize(deserializeItr);
+
+		DLOG("\tUndo restored {} serialized bytes", data.size());
+
+		noteGraphHistory.pop_front();
+		return true;
+	} else {
+		DLOG("\tNothing left to undo.");
+		return false;
+	}
 }
