fde7236a3af4cb54cd2333fe034ad2520cf2e1a4
Zealan <zlaferte@gmail.com>
2022-05-08 11:01:23 -0700

    Update/sync Core.cpp

--- src/Core/Core.cpp
+++ src/Core/Core.cpp
@@ -7,16 +7,38 @@
 #include "UI/UI.h"
 #include "MIDI/MIDI.h"
 
+
 NoteGraph::RenderContext GetNoteGraphRenderCtx() {
 	NoteGraph::RenderContext ctx;
 	ctx.fullNoteGraphScreenArea = Area(
-		0,
+		Vec(0, ImGui::GetFrameHeight()), // Pad for top menu bar
 		Renderer::GetWindowSize()
 	);
 	return ctx;
 }
 
+#include "../Render/Draw/Draw.h"
 void Core::OnRender() {
+	// Test stuff
+	if (g_NoteGraph.GetNoteCount() == 0) {
+
+		std::ifstream midiDataIn("C:\\Users\\admin\\Downloads\\Mephisto waltz_Duepree10.MID", std::ios::binary);
+		assert(midiDataIn.good());
+		ByteDataStream inData;
+		inData.ReadFromFileStream(midiDataIn);
+
+		MIDIParseData parseData;
+		if (MIDI::ParseMidi(inData.GetIterator(), parseData)) {
+			DLOG("Notes read: {}", parseData.notes.size());
+		} else {
+			assert(false);
+		}
+
+		for (Note note : parseData.notes) {
+			g_NoteGraph.AddNote(note);
+		}
+	}
+
 	auto renderCtx = GetNoteGraphRenderCtx();
 	g_NoteGraph.Render(&renderCtx);
 }
@@ -29,6 +51,8 @@ void Core::OnUserExit() {
 void Core::ProcessEvent(SDL_Event& e) {
 	if (UI::PreventInput())
 		return;
+
+	bool handledByAction = false;
 
 	switch (e.type) {
 	case SDL_QUIT:
@@ -48,8 +72,8 @@ void Core::ProcessEvent(SDL_Event& e) {
 		kbFlags |= KBFLAG_SHIFT * ((keyMods & KMOD_SHIFT) > 0);
 		kbFlags |= KBFLAG_ALT * ((keyMods & KMOD_ALT) > 0);
 
-		bool handledByAction = false;
-		for (Action* action : g_Actions.actions) {
+		for (auto& pair : g_Actions.actions) {
+			auto action = pair.second;
 			if (action->bind.key == pressedKey && action->bind.flags == kbFlags) {
 				if (action->undoable)
 					g_History.Update();
@@ -70,5 +94,9 @@ void Core::ProcessEvent(SDL_Event& e) {
 	}
 
 	auto renderCtx = GetNoteGraphRenderCtx();
-	g_NoteGraph.UpdateWithInput(e, &renderCtx);
+	Area inputArea = renderCtx.fullNoteGraphScreenArea;
+	// Only events to the note graph if they are within the screen area and no ImGui menu is active
+	if (g_MousePos >= inputArea.min && g_MousePos <= inputArea.max && !g_WasImGuiMenuActive)
+		g_NoteGraph.UpdateWithInput(e, &renderCtx);
+
 }
